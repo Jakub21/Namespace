277f3fbe1bc2b7815e89dfc62b7a6f09628557c1
Jakub21 <jakubp2101@gmail.com>
2020-01-28 20:15:41 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'SimpleNs',
+  description = 'SimpleNs',
+  version = '1.0.0',
+  license = 'MIT',
+  author = 'Jakub21',
+  url = 'https://github.com/Jakub21',
+  packages = find_packages(),
+)
