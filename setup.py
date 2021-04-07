f3f1b0d390c68f15583a0a27eaca774a108c57af
--local <jakubp2101@gmail.com>
2021-04-07 20:29:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
   name = 'javascript-namespaces',
   description = 'Namespace',
-  version = '1.1.0',
+  version = '1.2.0',
   license = 'MIT',
   author = 'Jakub21',
-  url = 'https://github.com/Jakub21',
+  url = 'https://github.com/Jakub21/Namespace',
   packages = find_packages(),
 )
